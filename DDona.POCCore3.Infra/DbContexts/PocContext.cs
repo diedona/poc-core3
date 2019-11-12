ee9a413d43283e8c6026cf9167d5a649d31b21f6
Diego Doná <diedona@gmail.com>
2019-11-12 19:32:20 -0300

    .net standard 2.1 + start infra

---
+++ DDona.POCCore3.Infra/DbContexts/PocContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDona.POCCore3.Infra.DbContexts
+{
+    public class PocContext : DbContext
+    {
+
+    }
+}
